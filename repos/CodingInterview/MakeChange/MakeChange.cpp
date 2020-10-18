578a5f628cbaddc95de72986ba0df3cf55584db8
Yong Ju Kwak <amese1225@gmail.com>
2020-10-18 15:31:15 +0900

    잔돈만들기, 격자판에 Queen 올리기

---
+++ repos/CodingInterview/MakeChange/MakeChange.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int makeChange(int amount, vector<int> changes, int index, vector<vector<int>>& memo) {
+    if (memo[amount][index] > 0)
+        return memo[amount][index];
+
+    if (index >= changes.size() - 1)
+        return 1;
+
+    int changeAmount = changes[index];
+    int way = 0;
+
+    for (int i = 0; i * changeAmount <= amount; i++) {
+        int remain = amount - changeAmount * i;
+        way += makeChange(remain, changes, index + 1, memo);
+    }
+
+    memo[amount][index] = way;
+    return way;
+}
+
+int makeChange(int n) {
+    vector<int> changes = { 25, 10, 5, 1 };
+    vector<vector<int>> map(n + 1, vector<int>(changes.size(), 0));
+
+    return makeChange(n, changes, 0, map);
+}
+
+
+
+int main()
+{
+    int amount = makeChange(120);
+
+    cout << amount << endl;
+    std::cout << "Hello World!\n";
+}
---
+++ repos/CodingInterview/NQueen/NQueen.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const int grid_size = 4;
+
+bool checkValid(vector<int> colums, int row1, int col1) {
+    for (int row2 = 0; row2 < row1; row2++) {
+        int col2 = colums[row2];
+
+        if (col1 == col2)
+            return false;
+
+        int col_dis = abs(col2 - col1);
+        int row_dis = row1 - row2;
+
+        if (col_dis == row_dis)
+            return false;
+    }
+
+    return true;
+}
+
+
+void placeQueens(int row, vector<int> &columns, vector<vector<int>>& result) {
+    if (row == grid_size)
+        result.push_back(columns);
+    else {
+        for (int col = 0; col < grid_size; col++) {
+            if (checkValid(columns, row, col)) {
+                columns[row] = col;
+                placeQueens(row + 1, columns, result);
+            }
+        }
+    }
+}
+
+
+
+int main()
+{
+    int answer = 0;
+    vector<int> columns(grid_size);
+    vector<vector<int>> result;
+
+    placeQueens(0, columns, result);
+    cout << result.size();
+    std::cout << "Hello World!\n";
+}
