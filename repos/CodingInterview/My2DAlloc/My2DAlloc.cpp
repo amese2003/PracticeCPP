36e5f17343eaab35d775d0c44ef94d462cc89c75
Yong Ju Kwak <amese1225@gmail.com>
2021-01-11 15:30:30 +0900

    C의 2차원 malloc 구현

---
+++ repos/CodingInterview/My2DAlloc/My2DAlloc.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+
+int** my2DAlloc(int row, int col) {
+    int i;
+
+    int header = row * sizeof(int*);
+    int data = row * col * sizeof(int);
+
+    int** rowptr = (int**)malloc(header + data);
+    if (rowptr == NULL) return NULL;
+
+    int* buf = (int*)(rowptr + row);
+    for (int i = 0; i < row; i++)
+        rowptr[i] = buf + i * col;
+
+    return rowptr;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+    int** test = my2DAlloc(3, 3);    
+    free(test);
+}
+
