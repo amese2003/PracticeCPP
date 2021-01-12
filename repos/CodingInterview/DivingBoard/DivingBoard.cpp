c1c2a7961c37116386561e546b0d4b90bb97180e
Yong Ju Kwak <amese1225@gmail.com>
2021-01-12 11:58:37 +0900

    unordered_set으로 변경

--- repos/CodingInterview/DivingBoard/DivingBoard.cpp
+++ repos/CodingInterview/DivingBoard/DivingBoard.cpp
@@ -1,24 +1,28 @@
 ﻿#include <iostream>
 #include <vector>
 #include <string>
-#include <hash_set>
+#include <unordered_set>
 
 using namespace std;
 
-hash_set<int> AllLength(int k, int shorter, int longer) {
-    hash_set<int> len;
-    GetAllLength(k, 0, shorter, longer, len);
+unordered_set<int> AllLength(int k, int shorter, int longer) {
+    unordered_set<int> len;
+    for (int nShorter = 0; nShorter <= k; nShorter++) {
+        int nLonger = k - nShorter;
+        int length = nShorter * shorter + nLonger * longer;
+        len.insert(length);
+    }
     return len;
 }
-void GetAllLength(int k, int total, int shorter, int longer, hash_set<int>& length) {
-    if (k == 0) {
-        length.insert(total);
-        return;
-    }
-
-    GetAllLength(k - 1, total + shorter, shorter, longer, length);
-    GetAllLength(k - 1, total + longer, shorter, longer, length);
-}
+//void GetAllLength(int k, int total, int shorter, int longer, hash_set<int>& length) {
+//    if (k == 0) {
+//        length.insert(total);
+//        return;
+//    }
+//
+//    GetAllLength(k - 1, total + shorter, shorter, longer, length);
+//    GetAllLength(k - 1, total + longer, shorter, longer, length);
+//}
 
 
 int main()
