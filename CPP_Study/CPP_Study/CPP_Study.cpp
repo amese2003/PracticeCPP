564a872cb358d0141e451f2f5f224ce73fe3c50c
Yong Ju Kwak <amese1225@gmail.com>
2021-02-20 16:33:24 +0900

    비트 연산

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -21,35 +21,74 @@ using namespace std;
 // - 매~~~우 예외적인 고대 문자만 4바이트 (사실상 무시해도 됨)
 
 // wchar_t test = L'안';
-wchar_t test = 0xc548;
-wchar_t wch = '\'';
 
+unsigned char flag; // 부호를 없애야 >> 를 해도 부호가 딸려오지 않음!
 
-
-// Esacape Sequence
-// 표기하기 애매한 애들을 표현
-// \0 = 아스키코드 0 = NULL
-// \t = 아스키코드 9 = Tab
-// \n = 아스키코드 10 = LineFeed (한줄 아래로)
-// \r = 아스키코드 13 = CarriageReturn (커서 <<)
-// \'
-
-// 문자열
-// 문자들이 열을 지어서 모여 있는 것 (문자 배열?)
-// 정수 (1~8바이트) 고정 길이로
-// Ex) Hello World
-char str[] = { 'h', 'e', 'l', 'l', 'o', '\0' };
-char str2[] = "Hello World";
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    bool a = 1;
+
+#pragma region 비트연산
+    
+    // 언제 필요할까?
+    // 비트 단위의 조작이 필요할 때
+    // - 대표적으로 BitFlag
+
+    // ~ bitwise not
+    // 단일 숫자의 모든 비트를 대상으로, 0은 1, 1은 0으로
+
+    // & bitwise and
+    // 두 숫자의 모든 비트 쌍을 대상으로, and
+
+    // | bitwise or
+    // 두 숫자의 모든 비트 쌍을 대상으로, or
+
+    // ^ bitwise xor
+    // 두 숫자의 모든 비트 쌍을 대상으로, xor
+
+    // << 비트 좌측 이동
+    // 비트열을 N만큼 왼쪽으로 이동
+    // 넘치는 비트는 버림.
+    // *2를 할 때 자주 보임.
+
+    // >> 비트 우측 이동
+    // 비트열을 N만큼 오른쪽으로 이동
+    // 오른쪽의 넘치는 N개의 비트는 버림.
+    // 왼쪽 생성되는 N개의 비트는 어떻게 할래?
+    // - 부호 비트가 존재할 경우 부호 비트를 따라감 (부호있는 정수라면 이 부분을 유의)
+    // - 아니면 0
+
+    int a = 1;
+    int b = 123;
+
+    // b가 대칭키!
+    a ^= b;
+    a ^= b;
+
+    // 실습
+    // 0b0000 [무적][변이][스턴][공중부양]
+
+    // 무적?
+    flag = (1 << 3);
+    
+    // 변이 상태 추가 (무적 + 변이)
+    flag |= (1 << 2);
+
+    // 무적인지 확인하고 싶다
+    // bitmask
+    bool invincible = ((flag & (1 << 3)) == 1);
+    
+    
+    // 무적이거나 스턴상태
+    bool stunOrInvincible = ((flag & 0b1010) != 0);
+
+
+
     
 
-    cout << str << endl;
 
-    wcout.imbue(locale("kor"));
-    wcout << wch << endl;
+#pragma endregion
 
 
     return 0;
