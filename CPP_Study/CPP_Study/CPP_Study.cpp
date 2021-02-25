7e6a4675ca51efc4ace5012e1eb63b2bfddc550b
Yong Ju Kwak <amese1225@gmail.com>
2021-02-25 22:50:46 +0900

    다중포인터

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,111 +1,45 @@
 ﻿#include <iostream>
-#include <unordered_set>
 
 using namespace std;
 
-// 오늘의 주제 : 로또 번호 생성기
+// 오늘의 주제 : 다중 포인터
 
-void Swap(int& a, int& b);
-void Sort(int numbers[], int count);
-void ChooseLotto(int numbers[]);
+void SetMessage(const char* a) {
+	a = "bye";
+}
+
+void SetMessage(const char** a) {
+	*a = "Bye";
+}
 
 
+void SetMessage2(const char*& a) {
+	a = "Wow";
+}
 
 int main() {	
 
-	// 1) Swap 함수 만들기
+	int a = 0;
 
-	int a = 1;
-	int b = 2;
+	const char* msg = "Hello";
+	SetMessage(msg);
 
-	const int count = 6;
-	int numbers[count] = { 1, 42, 3, 15, 5, 6 };
-	ChooseLotto(numbers);	
+	//cout << msg << endl; // Hello? bye?
+
+	// 주소2 [] << 1바이트 바구니
+	// 주소1 [ 주소2  ] << 8바이트
+	// pptr[ 주소1 ] << 8바이트
+	const char** pptr = &msg;
+
+
+	// [매개변수][ret][지역변수(msg(Hello 주소))][매개변수(a(&msg))][ret][지역변수]
+	SetMessage(&msg);
+	cout << msg << endl;
+
+	// 다중 포인터 : 혼동스럽다?
+	// 그냥 양파까기라고 생각하면 된다.
+	SetMessage2(msg);
+	cout << msg << endl;
 
 	return 0;
-}
-
-void Swap(int& a, int& b) {
-
-	int temp = a;
-	a = b;
-	b = temp;
-}
-
-void Sort(int numbers[], int count) {
-	for (int pivot = 0; pivot < count; pivot++) {
-		for (int swap = 0; swap < count - 1 - pivot; swap++) {
-			if (numbers[swap] > numbers[swap + 1]) {
-				int temp = numbers[swap];
-				numbers[swap] = numbers[swap + 1];
-				numbers[swap + 1] = temp;
-			}
-		}
-	}
-
-	for (int i = 0; i < count; i++) {
-		cout << numbers[i] << " ";
-	}
-
-	cout << "\n";
-
-	/*cout << sizeof(numbers) << endl;
-	int size = count;
-
-
-	int pivot = 0;
-	int start = pivot + 1;
-	int end = size - 1;*/
-
-	// 1, 42, 3, 15, 5, 6
-	// 1, 6, 3, 15, 5, 42
-	// 1, 5, 3, 15, 6, 42
-
-	// 1, 5, 3  15, 6, 42
-	// 1, 3, 5  6, 15, 42
-
-
-
-	/*while (start < end) {
-		if (*(numbers + start) > *(numbers + end)) {
-
-			int temp = *(numbers + start);
-			*(numbers + start) = *(numbers + end);
-			*(numbers + end) = temp;
-
-			end--;
-		}
-
-		if (*(numbers + start) < *(numbers + end)) {
-			start++;
-		}
-	}
-
-
-	if (start >= end && *(numbers + pivot) > *(numbers + start)) {
-		int temp = *(numbers + pivot);
-		*(numbers + pivot) = *(numbers + start);
-		*(numbers + start) = temp;
-	}*/
-}
-
-void ChooseLotto(int numbers[]) {
-	srand((unsigned)time(0));
-
-	int count = 0;
-
-	unordered_set<int> lotto;
-
-	while (count < 6) {
-		int num = 1 + (rand() % 45);		
-
-		if (lotto.find(num) == lotto.end()) {
-			*(numbers + count) = num;
-			lotto.insert(num);
-			count++;
-		}	
-	}
-
-
-	Sort(numbers, lotto.size());
 }
