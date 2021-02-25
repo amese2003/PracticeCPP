9a33024a2749ed896cb7915661a524e61ea98d88
Yong Ju Kwak <amese1225@gmail.com>
2021-02-25 22:08:34 +0900

    로또 번호 생성기

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,44 +1,111 @@
 ﻿#include <iostream>
+#include <unordered_set>
 
 using namespace std;
 
-// 오늘의 주제 : 포인터 vs 배열
+// 오늘의 주제 : 로또 번호 생성기
 
-void Test(int* a) {
-	*a += 1;
-	cout << *a << endl;
-}
+void Swap(int& a, int& b);
+void Sort(int numbers[], int count);
+void ChooseLotto(int numbers[]);
 
-// 배열은 함수 인자로 넘기면, 컴파일러가 알아서 포인터로 치환한다 (char[] -> char *)
-// 즉 배열 내용을 넘긴게 아니라, 시작 주소를 넘긴다
-void Test(char a[]) {
-	a[0] = 'x';
-}
 
 
 int main() {	
 
-	// .data 주소 [H][e][l][l][o][][W][o][r][l][d][\n];
-	// test1[ 주소 ] << 8바이트
-	const char* test1 = "Hello World";
+	// 1) Swap 함수 만들기
 
-	// .data 주소 [H][e][l][l][o][][W][o][r][l][d][\n];
-	// [H][e][l][l] [o][][W][o] [r][l][d][\n];
-	char test2[] = "Hello World";
-	test2[0] = 'R';
+	int a = 1;
+	int b = 2;
 
-	// 포인터는 [주소를 담는 바구니]
-	// 배열은 [닭장] 즉, 그 자체로 같은 데이터끼리 붙어있는 '바구니' 모음
-	// - 다만 [배열 이름]은 '바구니 모음'의 [시작주소]
+	const int count = 6;
+	int numbers[count] = { 1, 42, 3, 15, 5, 6 };
+	ChooseLotto(numbers);	
 
-	// 배열을 함수의 인자로 넘기게 되면?
-	int a = 0;
-	Test(&a);
-	Test(test2);
-
-	cout << test1 << endl;
-
-	cout << test2 << endl;
-	
 	return 0;
+}
+
+void Swap(int& a, int& b) {
+
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void Sort(int numbers[], int count) {
+	for (int pivot = 0; pivot < count; pivot++) {
+		for (int swap = 0; swap < count - 1 - pivot; swap++) {
+			if (numbers[swap] > numbers[swap + 1]) {
+				int temp = numbers[swap];
+				numbers[swap] = numbers[swap + 1];
+				numbers[swap + 1] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < count; i++) {
+		cout << numbers[i] << " ";
+	}
+
+	cout << "\n";
+
+	/*cout << sizeof(numbers) << endl;
+	int size = count;
+
+
+	int pivot = 0;
+	int start = pivot + 1;
+	int end = size - 1;*/
+
+	// 1, 42, 3, 15, 5, 6
+	// 1, 6, 3, 15, 5, 42
+	// 1, 5, 3, 15, 6, 42
+
+	// 1, 5, 3  15, 6, 42
+	// 1, 3, 5  6, 15, 42
+
+
+
+	/*while (start < end) {
+		if (*(numbers + start) > *(numbers + end)) {
+
+			int temp = *(numbers + start);
+			*(numbers + start) = *(numbers + end);
+			*(numbers + end) = temp;
+
+			end--;
+		}
+
+		if (*(numbers + start) < *(numbers + end)) {
+			start++;
+		}
+	}
+
+
+	if (start >= end && *(numbers + pivot) > *(numbers + start)) {
+		int temp = *(numbers + pivot);
+		*(numbers + pivot) = *(numbers + start);
+		*(numbers + start) = temp;
+	}*/
+}
+
+void ChooseLotto(int numbers[]) {
+	srand((unsigned)time(0));
+
+	int count = 0;
+
+	unordered_set<int> lotto;
+
+	while (count < 6) {
+		int num = 1 + (rand() % 45);		
+
+		if (lotto.find(num) == lotto.end()) {
+			*(numbers + count) = num;
+			lotto.insert(num);
+			count++;
+		}	
+	}
+
+
+	Sort(numbers, lotto.size());
 }
