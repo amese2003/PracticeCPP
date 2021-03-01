a92ba0990118b581972449063081fc60e02a30cc
Yong Ju Kwak <amese1225@gmail.com>
2021-03-01 19:09:54 +0900

    pragma의 이해와 파일 관리

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,88 +1,32 @@
 ﻿#include <iostream>
 #include <iomanip>
 using namespace std;
+#include "Test1.h"
+#include "Test2.h"
 
-// 오늘의 주제 : 연습문제
+// 오늘의 주제 : 파일 분할 관리
 
-const int MAX = 100;
-int board[MAX][MAX] = {};
-int N;
+#ifndef  _TEST1_H__
+#define _TEST1_H__
 
-enum Dir {
-	RIGHT = 0,
-	DOWN = 1,
-	LEFT = 2,
-	UP = 3	
+struct StatInfo {
+	int hp;
+	int attack;
+	int defence;
 };
 
-void PrintBoard() {
+void Test_1();
 
-	for (int y = 0; y < N; y++) {
-		for (int x = 0; x < N; x++) {
-			cout << setfill('0') << setw(2) << board[y][x] << " ";
-		}
+void Test_2();
 
-		cout << endl;
-	}
-}
+void Test_3();
 
-
-bool CanGo(int y, int x) {
-
-	if (y < 0 || y >= N)
-		return false;
-
-	if (x < 0 || x >= N)
-		return false;
-
-	if (board[y][x] != 0)
-		return false;
-
-	return true;
-}
-
-
-
-void SetBoard() {
-	int dir = RIGHT;
-	int count = 1;
-	int y = 0;
-	int x = 0;
-
-
-	int dy[] = { 0, 1, 0, -1 };
-	int dx[] = { 1, 0, -1, 0 };
-
-	while(true) {		
-
-		board[y][x] = count;
-
-		if (count == N * N)
-			break;
-
-		int nextY = y + dy[dir];
-		int nextX = x + dx[dir];
-
-
-		if (CanGo(nextY, nextX)) {
-			y = nextY;
-			x = nextX;
-			count++;
-		}
-		else {
-			dir = (dir + 1) % 4;
-		}
-	}
-}
+#endif // ! _TEST1_H__
 
 
 int main() 
 {	
-	cin >> N;
-
-	SetBoard();
-	PrintBoard();
-
+	Test_2();
 
 	return 0;
 }
---
+++ CPP_Study/CPP_Study/Test1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "Test1.h"
+using namespace std;
+
+
+void Test_1() {
+	Test_2();
+}
+
+void Test_2() {
+	cout << "Hello World" << endl;
+}
+
+void Test_3() {
+
+}
