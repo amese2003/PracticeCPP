ff572e35e5a060c63c8d67e8df1a4f264c57cd72
Yong Ju Kwak <amese1225@gmail.com>
2021-03-01 20:27:06 +0900

    객체지향 시작

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,32 +1,68 @@
 ﻿#include <iostream>
 #include <iomanip>
 using namespace std;
-#include "Test1.h"
-#include "Test2.h"
 
-// 오늘의 주제 : 파일 분할 관리
+// 오늘의 주제 : 객체 지향 시작!
 
-#ifndef  _TEST1_H__
-#define _TEST1_H__
+// 데이터 + 가공 (로직, 동작)
 
-struct StatInfo {
+// 객체지향 = 객체
+// 객체란 ? 플레이어, 몬스터, GameRoom
+
+// knight를 설계해보자.
+// - 속성(데이터) : hp, attack, y, x .........
+// - 기능(동작) : Move, Attack, Die
+
+
+// class는 일종의 설계도
+class Knight {
+public:
+	// 멤버 함수 선언
+	void Move(int y, int x);
+	void Attack();
+	void Die() {
+		hp = 0;
+		cout << "Die" << endl;
+	}
+public:
 	int hp;
 	int attack;
-	int defence;
+	int posY;
+	int posX;
 };
 
-void Test_1();
+void Move(Knight* knight, int y, int x) {
+	knight->posY = y;
+}
 
-void Test_2();
+void Knight::Move(int y, int x) {
+	posY = y;
+	posX = x;
+	cout << "Move" << endl;
+}
 
-void Test_3();
-
-#endif // ! _TEST1_H__
-
+void Knight::Attack() {
+	cout << "Attack" << endl;
+}
 
 int main() 
 {	
-	Test_2();
+	Knight k1;
+	k1.hp = 100;
+	k1.attack = 10;
+	k1.posY = 0;
+	k1.posX = 0;
+
+	Knight k2;
+	k2.hp = 80;
+	k2.attack = 5;
+	k2.posY = 1;
+	k2.posX = 1;
+
+	k1.Move(2, 2);
+	k1.Attack();
+
+	k1.Die();
 
 	return 0;
 }
--- CPP_Study/CPP_Study/Test2.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#include "Test1.h"
