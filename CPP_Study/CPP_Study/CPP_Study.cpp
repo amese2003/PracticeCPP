575422b8af429730308496876f6b9ae0bef802a6
Yong Ju Kwak <amese1225@gmail.com>
2021-03-01 23:56:15 +0900

    생성자와 소멸자 #1

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,42 +2,80 @@
 #include <iomanip>
 using namespace std;
 
-// 오늘의 주제 : 객체 지향 시작!
+// 오늘의 주제 : 생성자와 소멸자
 
-// 데이터 + 가공 (로직, 동작)
+// 생성자 [Constructor] 와 소멸자 (Destructor)
+// 이 중에서 굉장히 특별한 함수 2종이 있는데, 바로 [시작]과 [끝]을 알리는 함수
 
-// 객체지향 = 객체
-// 객체란 ? 플레이어, 몬스터, GameRoom
+// - 시작 (탄생) -> 생성자
+// - 끝 (소멸) -> 소멸자 (딱 1개)
 
-// knight를 설계해보자.
-// - 속성(데이터) : hp, attack, y, x .........
-// - 기능(동작) : Move, Attack, Die
-
+// [암시적(Implicit) 생성자]
+// 생성자를 명시적으로 만들지 않으면?
+// 아무 인자도 받지 않는 [기본 생성자]가 컴파일러에 의해 자동으로 만들어짐.
+// -> 근데 우리가 명시적(Explicit)으로 아무 생성자 하나 만들면,
+// 자동으로 만들어지던 [기본 생성자]는 더 이상 만들어지지 않음!
 
 // class는 일종의 설계도
 class Knight {
 public:
+
+	// [1] 기본 생성자
+	Knight() {
+		_hp = 100;
+		_attack = 10;
+		_posY = 0;
+		_posX = 0;
+
+		cout << "Knight() 기본 생성자" << endl;
+	}
+
+	// [2] 복사 생성자? (자기 자신의 클래스 참조 타입을 인자로 받음)
+	// (일반적으로 '똑같은' 데이터를 지닌 객체가 생성되길 기대한다)
+	Knight(const Knight& knight) {
+		_hp = knight._hp;
+		_attack = knight._attack;
+		_posX = knight._posX;
+		_posY = knight._posY;
+	}
+
+
+	// [3] 기타 생성자
+	Knight(int hp) {
+		_hp = hp;
+		_attack = 10;
+		_posY = 0;
+		_posX = 0;
+
+		cout << "Knight() 기본 생성자" << endl;
+	}
+
+	// 소멸자
+	~Knight() {
+		cout << "~Knight() 소멸자" << endl;
+	}
+
 	// 멤버 함수 선언
 	void Move(int y, int x);
 	void Attack();
 	void Die() {
-		hp = 0;
+		_hp = 0;
 		cout << "Die" << endl;
 	}
 public:
-	int hp;
-	int attack;
-	int posY;
-	int posX;
+	int _hp;
+	int _attack;
+	int _posY;
+	int _posX;
 };
 
 void Move(Knight* knight, int y, int x) {
-	knight->posY = y;
+	knight->_posY = y;
 }
 
 void Knight::Move(int y, int x) {
-	posY = y;
-	posX = x;
+	_posY = y;
+	_posX = x;
 	cout << "Move" << endl;
 }
 
@@ -47,21 +85,15 @@ void Knight::Attack() {
 
 int main() 
 {	
-	Knight k1;
-	k1.hp = 100;
-	k1.attack = 10;
-	k1.posY = 0;
-	k1.posX = 0;
+	Knight k1(100);
+	k1._attack = 10;
+	k1._posY = 0;
+	k1._posX = 0;
 
-	Knight k2;
-	k2.hp = 80;
-	k2.attack = 5;
-	k2.posY = 1;
-	k2.posX = 1;
+	Knight k2(k1);
 
 	k1.Move(2, 2);
 	k1.Attack();
-
 	k1.Die();
 
 	return 0;
