ece2a21fa0ddca9b5cf7d2c9dbc901d5b039c73c
Yong Ju Kwak <amese1225@gmail.com>
2021-03-04 21:26:58 +0900

    동적할당 #2 (new/delete, new[] / delete[])

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -25,17 +25,39 @@ using namespace std;
 // - 그러면서도 (스택과는 다르게) 우리가 생성/소멸 시점을 관리할 수 있는!
 // - 그런 아름다운 메모리 없나? -> Heap 영역
 
-// 동적할당과 연관된 함수/연산자 : malloc, free, new, delete, new[], delete[]
+// 동적할당과 연관된 함수/연산자 : malloc/free, new/delete, new[]/delete[]
 
 // malloc
 // - 할당할 메모리 크기를 건네준다
 // - 메모리 할당 후 시작 주소를 가리키는 포인터를 반환한다. (메모리가 부족하면? NULL)
-
 // free
 // - malloc (혹은 기타 calloc, realloc 등의 사촌)을 통해 할당된 영역 해제
 // - 힙 관리자가 할당/미할당 여부를 구분해서 관리
 
+// new / delete
+// - c++ 추가
+// - malloc/free 함수! new/delete는 연산자.
+
+// new[] / delete[]
+// - new가 malloc에 비해 좋긴 한데... 배열과 같이 n개 데이터를 같이 할당할려면?
+
+// malloc/free vs new/delete
+// - 사용 편의성 -> new / delete 승!
+// - 타입에 상관없이 특정한 크기의 메모리 영역을 할당 받을려면? -> malloc/free 승!
+
+// 그런데 둘의 가장 가장 근본적인 중요한 차이는 따로 있다!
+// new / delete는 (생성타입이 클래스인 경우) 생성자 / 소멸자 호출!
+
+
 class Monster {
+public:
+	Monster() {
+		cout << "Monster()" << endl;
+	}
+
+	~Monster() {
+		cout << "~Monster()" << endl;
+	}
 public:
 	int _hp;
 	int _x;
@@ -75,16 +97,22 @@ int main()
 	// 만약에 free하지 않으면 메모리 누수
 	free(pointer);
 
-	// Double Free
-	// - 이건 그냥 대부분 크래시만 나고 끝;
-	//free(pointer);
+	Monster* m2 = new Monster;
+	m2->_hp = 200;
+	m2->_x = 2;
+	m2->_y = 3;
+	delete m2;
 
-	// Use-After-Free
-	// - 프로그래머 입장 : OMG 망했다!
-	// - 해커 입장 : 아싸!
-	m1->_hp = 100;
-	m1->_x = 1;
-	m1->_y = 2;
+	Monster* m3 = new Monster[5];
+	m3->_hp = 200;
+	m3->_x = 2;
+	m3->_y = 3;
+	delete[] m3;
+
+
+
+
+	delete m2;
 
 	return 0;
 }
