04a37aa075aaf7a7c14b56b470d928dc08a73cdd
Yong Ju Kwak <amese1225@gmail.com>
2021-03-05 22:42:43 +0900

    얕은 복사 vs 깊은 복사 #1

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,148 +2,81 @@
 #include <iomanip>
 using namespace std;
 
-// 오늘의 주제 : 타입 변환 (포인터)
+// 오늘의 주제 : 얕은 복사 vs 깊은 복사
+
+class Pet {
+public:
+	Pet() {
+		cout << "Pet()" << endl;
+	}
+
+	~Pet() {
+		cout << "~Pet()" << endl;
+	}
+
+	Pet(const Pet& pet) {
+		cout << "Pet(const Pet&)" << endl;
+	}
+};
+
 class Knight {
+public:
+	Knight() {
+			
+	}
 
+	Knight(const Knight& knight) {
+		_hp = knight._hp;
+		_pet = new Pet(*knight._pet); // 깊은 복사
+	}
+
+	Knight& operator=(const Knight& knight) {
+		_hp = knight._hp;
+		_pet = new Pet(*knight._pet); // 깊은 복사
+		return *this;
+	}
+
+	~Knight() {
+		delete _pet;
+	}
+
+public:
+	int _hp = 100;
+	Pet* _pet;
 };
-
-class Item {
-public:
-	Item() {
-		cout << "Item()" << endl;
-	}
-
-	Item(const Item& item) {
-		cout << "Item(const Item&)" << endl;
-	}
-
-	Item(int itemType) : _itemType(itemType) {
-
-	}
-
-	~Item() {
-		cout << "~Item()" << endl;
-	}
-
-
-public:
-	int _itemType = 0;
-	int _itemDbId = 0;
-
-	char _dummy[4096] = {}; // 이런 저런 정보들로 인해 비대해짐
-};
-
-enum ItemType {
-	IT_WEAPON = 1,
-	IT_ARMOR = 2
-};
-
-class Weapon : public Item
-{
-public:
-	Weapon() : Item(IT_WEAPON)
-	{
-		_itemType = 1;
-		cout << "Item()" << endl;
-	}
-
-	~Weapon() {
-		cout << "~Weapon()" << endl;
-	}
-
-public:
-	int _damage = 0;
-};
-
-class Armor : public Item {
-public:
-	Armor() : Item(IT_ARMOR)
-	{
-		cout << "Item()" << endl;
-	}
-
-	~Armor() {
-		cout << "~Armor()" << endl;
-	}
-
-public:
-	int _defence = 0;
-};
-
-void TestItem(Item item) {
-
-}
-
-void TestItmePtr(Item* item) {
-
-}
 
 int main() 
 {	
-	// 연관성이 없는 클래스 사이의 포인터 변환 테스트
-	{
-		// Stack [ 주소 ] -> Heap [_hp(4)]
-		Knight* knight = new Knight();
+	Pet* pet = new Pet();
 
-		// 암시적은 안돼
-		// 명시적   가능
+	Knight knight; // 기본 생성자
+	knight._hp = 200;
+	knight._pet = pet;
 
-		// stack [주소] 
-		Item* item = (Item*)knight;
-		item->_itemType = 2;
-		item->_itemDbId = 1;
-	}
+	Knight knight2 = knight; // 복사 생성자
+	//Knight knight3(knight);
 
-	// 자식 -> 부모 변환 테스트
-	{
-		Item* item = new Item();
+	Knight knight3; // 기본 생성자
+	knight3 = knight; // 복사 대입 생성자
 
+	// [복사 생성자] + [복사 대입 연산자]
+	// 둘 다 안 만들어주면 컴파일러가 '암시적으로' 만들어준다
 
-		// [    [Item]      ]
-		// [ _damage        ]
-		//Weapon* weapon = (Weapon*)item;
+	// 중간 결론 ) 컴파일러가 알아서 잘 만들어준다?
+	// 수고. 다음주제로 넘아갈까? << 안돼!
 
-		// 으악 안돼
-		//weapon->_damage = 10;
+	// [ 얕은 복사 Shallow Copy ]
+	// 멤버 데이터를 비ㅡ열 단위로 '똑같이' 복사 (메모리 영역값도 복사)
+	// 포인터는 주소값 바구니 -> 주소값을 똑같이 복사 -> 동일한 객체를 가리키는 상태가 됨
+	// Stack : Knight1 [ hp 0x1000 ] -> Heap 0x1000 pet[ ];
+	// Stack : Knight2 [ hp 0x1000 ]
+	// Stack : Knight2 [ hp 0x1000 ] 
 
-
-		delete item;
-	}
-
-	// 부모 -> 자식 변환 테스트
-	{
-		// [    [Item]      ]
-		// [ _damage        ]
-		Weapon* weapon = new Weapon();
-
-		// 암시적으로도 가능!
-		Item* item = weapon;
-
-		delete weapon;
-	}
-	// 명시적으로 타입 변환 할 때는 항상 조심해야한다!
-	// 암시적으로 될 때는 안전하다?
-	// -> 평생 명시적으로 타입 변환(캐스팅)은 안 하면 되는거 아님?
-
-	Item* inventory[20] = {};
-
-	srand((unsigned int)time(0));
-	for (int i = 0; i < 20; i++) {
-		int randvalue = rand() & 2;
-
-		switch (randvalue)
-		{
-		case 0:
-			inventory[i] = new Weapon();
-			break;
-		case 1:
-			inventory[i] = new Armor();
-			break;
-
-		default:
-			break;
-		}
-	}
-
+	// [ 깊은 복사 Deep Copy ]
+	// 멤버 데이터가 참조(주소) 값이라면, 데이터를 새로 만들어준다 (원본 객체가 참조하는 대상까지 새로 만들어서 복사)
+	// 포인터는 주소값 바구니 -> 새로운 객체를 생성 ->
+	// Stack : Knight1 [ hp 0x1000 ] -> Heap 0x1000 pet[ ];
+	// Stack : Knight2 [ hp 0x1004 ] -> Heap 0x1004 pet[ ];
+	// Stack : Knight2 [ hp 0x1008 ] -> Heap 0x1008 pet[ ];
 	return 0;
 }
