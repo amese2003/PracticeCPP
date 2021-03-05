53fc4b136bcee9bf230b1393d8d60088f30d4e2f
Yong Ju Kwak <amese1225@gmail.com>
2021-03-05 20:51:32 +0900

    타입 변환 #4

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,6 +3,9 @@
 using namespace std;
 
 // 오늘의 주제 : 타입 변환 (포인터)
+class Knight {
+
+};
 
 class Item {
 public:
@@ -12,6 +15,10 @@ public:
 
 	Item(const Item& item) {
 		cout << "Item(const Item&)" << endl;
+	}
+
+	Item(int itemType) : _itemType(itemType) {
+
 	}
 
 	~Item() {
@@ -26,6 +33,43 @@ public:
 	char _dummy[4096] = {}; // 이런 저런 정보들로 인해 비대해짐
 };
 
+enum ItemType {
+	IT_WEAPON = 1,
+	IT_ARMOR = 2
+};
+
+class Weapon : public Item
+{
+public:
+	Weapon() : Item(IT_WEAPON)
+	{
+		_itemType = 1;
+		cout << "Item()" << endl;
+	}
+
+	~Weapon() {
+		cout << "~Weapon()" << endl;
+	}
+
+public:
+	int _damage = 0;
+};
+
+class Armor : public Item {
+public:
+	Armor() : Item(IT_ARMOR)
+	{
+		cout << "Item()" << endl;
+	}
+
+	~Armor() {
+		cout << "~Armor()" << endl;
+	}
+
+public:
+	int _defence = 0;
+};
+
 void TestItem(Item item) {
 
 }
@@ -36,41 +80,69 @@ void TestItmePtr(Item* item) {
 
 int main() 
 {	
-	// 복습
+	// 연관성이 없는 클래스 사이의 포인터 변환 테스트
 	{
-		// Stack [ type(4) dbid(4) dummy(4096) ]
-		Item item;
+		// Stack [ 주소 ] -> Heap [_hp(4)]
+		Knight* knight = new Knight();
 
-		// Stack [ 주소 (4,8) ] -> Heap 주소 [ type(4) dbid(4) dummy(4096) ]
-		Item* item2 = new Item();
+		// 암시적은 안돼
+		// 명시적   가능
 
-		TestItem(item);
-		TestItem(*item2);
-
-		TestItmePtr(&item);
-		TestItmePtr(item2);
-
-		// 메모리 누수 (Memory Leak) -> 점점 가용 메모리가 줄어든다!
-		delete item2;
+		// stack [주소] 
+		Item* item = (Item*)knight;
+		item->_itemType = 2;
+		item->_itemDbId = 1;
 	}
 
-	// 배열
+	// 자식 -> 부모 변환 테스트
 	{
-		cout << "----------------------" << endl;
-		// 진짜 아이템이 100개 있음. stack 메모리에 올라와있다
-		Item item3[100] = {};
-		
-		cout << "----------------------" << endl;
-		// 아이템이 100개가 있을까?
-		// 아이템을 가리키는 바구나가 100개. 실제 아이템은 1개도 없을수도...
-		Item* item4[100] = {};
+		Item* item = new Item();
 
-		for (int i = 0; i < 100; i++)
-			item4[i] = new Item();
-		cout << "----------------------" << endl;
 
-		for (int i = 0; i < 100; i++)
-			delete item4[i];
+		// [    [Item]      ]
+		// [ _damage        ]
+		//Weapon* weapon = (Weapon*)item;
+
+		// 으악 안돼
+		//weapon->_damage = 10;
+
+
+		delete item;
+	}
+
+	// 부모 -> 자식 변환 테스트
+	{
+		// [    [Item]      ]
+		// [ _damage        ]
+		Weapon* weapon = new Weapon();
+
+		// 암시적으로도 가능!
+		Item* item = weapon;
+
+		delete weapon;
+	}
+	// 명시적으로 타입 변환 할 때는 항상 조심해야한다!
+	// 암시적으로 될 때는 안전하다?
+	// -> 평생 명시적으로 타입 변환(캐스팅)은 안 하면 되는거 아님?
+
+	Item* inventory[20] = {};
+
+	srand((unsigned int)time(0));
+	for (int i = 0; i < 20; i++) {
+		int randvalue = rand() & 2;
+
+		switch (randvalue)
+		{
+		case 0:
+			inventory[i] = new Weapon();
+			break;
+		case 1:
+			inventory[i] = new Armor();
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	return 0;
