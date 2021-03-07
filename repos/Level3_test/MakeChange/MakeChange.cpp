cdd281464d98ad919d0246099bb3262238f8b5c9
Yong Ju Kwak <amese1225@gmail.com>
2021-03-07 19:02:45 +0900

    코딩 테스트 - 잔돈 만들기

---
+++ repos/Level3_test/MakeChange/MakeChange.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> money) 
+{
+    int answer = 0;
+
+    vector<int> MakeChange(n + 1);
+
+    MakeChange[0] = 1;
+
+    for (int i = 0; i < money.size(); i++) {
+
+        int change = money[i];
+
+        for (int j = change; j <= n; j++) {
+            MakeChange[j] += MakeChange[j - change];
+        }
+    }
+
+
+    return MakeChange[n];
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    
+
+    //int test = solution(5, { 1,2,5 });
+
+
+}
