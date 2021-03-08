3f909d087827164504c20cb43d26a8f2983b1ae1
Yong Ju Kwak <amese1225@gmail.com>
2021-03-08 10:11:35 +0900

    코딩 테스트 - 보석 쇼핑

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -2,140 +2,75 @@
 #include <iomanip>
 using namespace std;
 
-// 오늘의 주제 : 얕은 복사 vs 깊은 복사
-
-class Pet {
-public:
-	Pet() {
-		cout << "Pet()" << endl;
-	}
-
-	~Pet() {
-		cout << "~Pet()" << endl;
-	}
-
-	Pet(const Pet& pet) {
-		cout << "Pet(const Pet&)" << endl;
-	}
-
-	Pet& operator=(const Pet& pet) {
-		cout << "operator=(const Pet&)" << endl;
-		return *this;
-	}
-};
+// 오늘의 주제 : 캐스팅 (타입 변환)
 
 class Player {
 public:
-	Player() {
-		cout << "Player()" << endl;
-	}
+	virtual ~Player() {
 
-	Player(const Player& player) {
-		cout << "Player(const Player&)" << endl;
-		_level = player._level;
 	}
+};
 
-	Player& operator=(const Player& player) {
-		cout << "operator=(const Player&)" << endl;
-		_level = player._level;
-		return *this;
-	}
-
-public:
-	int _level = 0;
+class Dog {
 
 };
 
 class Knight : public Player {
 public:
-	Knight() {
-			
-	}
-
-	Knight(const Knight& knight) : Player(knight), _pet(knight._pet) {
-		_hp = knight._hp;
-	}
-
-	
-	Knight& operator=(const Knight& knight) {
-
-		cout << "operator=(const Knight&)" << endl;
-
-		Player::operator=(knight);
-		_pet = knight._pet;
-
-		_hp = knight._hp;
-		return *this;
-	}
-
-	~Knight() {
-	}
-
-public:
-	int _hp = 100;
-	Pet _pet;
 };
+
+class Archer : public Player {
+public:
+};
+
+void PrintName(char* str) {
+	cout << str << endl;
+}
+
+// 1) static_cast
+// 2) dynamic_cast
+// 3) const_cast
+// 4) reinterpret_cast
+
 
 int main() 
 {	
-	Pet* pet = new Pet();
+	// static_cast : 타입 원칙에 비춰볼 때 상식적인 캐스팅만 허용
+	// 1) int <-> float
+	// 2) Player* -> Knight* (다운 캐스팅 부모 -> 자식) << 단, 안전성은 보장 못함
 
-	Knight knight; // 기본 생성자
-	knight._hp = 200;
+	int hp = 100;
+	int maxHp = 200;
+	float ration = static_cast<float>(hp) / maxHp;
+
+	// 부모 -> 자식 -> 부모
+	Player* p = new Archer();
+	Knight* k1 = static_cast<Knight*>(p);
+
+	// dynamic_cast : 상속 관계에서의 안전 형변환
+	// RTTI ? (RunTime Type Information)
+	// 다형성을 활용하는 방식
+	// - virtual 함수를 하나라도 만들면, 객체의 메모리에 가상 함수 테이블 (vftable) 주소가 기입된다
+	// - 만약 잘못된 타입으로 캐스팅을 했으면, nullptr 반환
+	// 이를 이용해서 맞는 타입으로 캐스팅을 확인했는지 확인에 유용
+	Knight* k2 = dynamic_cast<Knight*>(p);
+
+	// const_cast : const를 붙이거나 떼거나..
+	PrintName(const_cast<char*>("Nero"));
+
+	// reinterpret_cast
+	// 가장 위험하고 강력한 형태의 캐스팅
+	// 're -interpret' : 다시-간주하다/생각하다
+	// - 포인터랑 전혀 관계 없는 다른 타입 변환
+	// - 포인터 -> 포인터
+	__int64 address = reinterpret_cast<__int64>(k2);
+
+	Dog* dog1 = reinterpret_cast<Dog*>(k2);
+
+	void* p = malloc(1000);
+	Dog* dog2 = reinterpret_cast<Dog*>(p);
 
 
-	//cout << "----------------------복사 생성자--------------------" << endl;
-	//Knight knight2 = knight; // 복사 생성자
-	//Knight knight3(knight);
-
-	
-	Knight knight3; // 기본 생성자
-	cout << "----------------------복사 대입 생성자--------------------------" << endl;
-	knight3 = knight; // 복사 대입 생성자
-
-	// [복사 생성자] + [복사 대입 연산자]
-	// 둘 다 안 만들어주면 컴파일러가 '암시적으로' 만들어준다
-
-	// 중간 결론 ) 컴파일러가 알아서 잘 만들어준다?
-	// 수고. 다음주제로 넘아갈까? << 안돼!
-
-	// [ 얕은 복사 Shallow Copy ]
-	// 멤버 데이터를 비ㅡ열 단위로 '똑같이' 복사 (메모리 영역값도 복사)
-	// 포인터는 주소값 바구니 -> 주소값을 똑같이 복사 -> 동일한 객체를 가리키는 상태가 됨
-	// Stack : Knight1 [ hp 0x1000 ] -> Heap 0x1000 pet[ ];
-	// Stack : Knight2 [ hp 0x1000 ]
-	// Stack : Knight2 [ hp 0x1000 ] 
-
-	// [ 깊은 복사 Deep Copy ]
-	// 멤버 데이터가 참조(주소) 값이라면, 데이터를 새로 만들어준다 (원본 객체가 참조하는 대상까지 새로 만들어서 복사)
-	// 포인터는 주소값 바구니 -> 새로운 객체를 생성 ->
-	// Stack : Knight1 [ hp 0x1000 ] -> Heap 0x1000 pet[ ];
-	// Stack : Knight2 [ hp 0x1004 ] -> Heap 0x1004 pet[ ];
-	// Stack : Knight2 [ hp 0x1008 ] -> Heap 0x1008 pet[ ];
-
-	// 실험
-	// - 암시적 복사 생성자 Steps
-	// 1) 부모 클래스의 복사 생성자 호출
-	// 2) 멤버 클래스의 복사 생성자 호출
-	// 3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사)
-
-	// - 명시적 복사 생성자 Steps
-	// 1) 부모 클래스의 기본 생성자 호출
-	// 2) 멤버 클래스의 기본 생성자 호출
-
-	// - 암시적 복사 대입 연산자 Steps
-	// 1) 부모 클래스의 복사 대입 연산자 호출
-	// 2) 멤버 클래스의 복사 대입 연산자 호출
-	// 3) 멤버가 기본 타입일 경우 메모리 복사 (얕은 복사)
-
-	// - 명시적 복사 대입 연산자 Steps
-	// 1) 알아서 해주는거 없다.
-
-	// 왜 이렇게 혼란스러울까?
-	// 객체를 '복사' 한다는 것은 두 객체의 값을 일치시키려는것
-	// 따라서 기본적으로는 얕은 복사 (Shallow Copy) 방식으로 동작
-
-	// 깊은 복사 -> 명시적 복사 -> [모든 책임]을 프로그래머한테 위임!
-
+	delete p;
 	return 0;
 }
---
+++ repos/Level3_test/JewelShopping/JewelShopping.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>;
+
+using namespace std;
+
+vector<int> solution(vector<string> gems) {
+    vector<int> answer(2, 0);
+
+    unordered_map<string, int> gemMap;
+    unordered_set<string> gemSet(gems.begin(), gems.end());
+
+    int minSize = 0;
+    int startPos = 0;
+    int endPos = 0;
+
+    for (int i = 0; i < gems.size(); i++) {
+        gemMap[gems[i]]++;
+
+        if (gemMap.size() == gemSet.size())
+            break;
+
+        endPos++;
+    }
+
+    minSize = endPos - startPos;
+    answer[0] = startPos + 1;
+    answer[1] = endPos + 1;
+
+    while (endPos < gems.size()) {
+        string key = gems[startPos];
+        gemMap[key]--;
+        startPos++;
+
+        if (gemMap[key] == 0) {
+            endPos++;
+
+            while (endPos < gems.size()) {
+                gemMap[gems[endPos]]++;
+
+                if (key == gems[endPos])
+                    break;
+
+                endPos++;
+            }
+            
+            if (endPos == gems.size())
+                break;
+        }
+
+        if (endPos - startPos < minSize) {
+            answer[0] = startPos + 1;
+            answer[1] = endPos + 1;
+            minSize = endPos - startPos;
+        }
+    }
+
+
+    return answer;
+}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    solution({ "DIA", "RUBY", "RUBY", "DIA", "DIA", "EMERALD", "SAPPHIRE", "DIA" });
+}
