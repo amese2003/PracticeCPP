3ee07cc2b63693133d4f84f7f488b1d317d5cb70
Yong Ju Kwak <amese1225@gmail.com>
2021-03-10 23:19:17 +0900

    템플릿 기초 #1 (함수 템플릿)

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -1,95 +1,71 @@
 ﻿#include <iostream>
 using namespace std;
 
-// 오늘의 주제 : 함수 객체
-
-void HelloWorld() {
-
-}
-
-void HelloNumber(int number) {
-
-}
+// 오늘의 주제 : 템플릿 기초
 
 class Knight {
 public:
-	void AddHp(int addHp) {
-		_hp += addHp;
-	}
+	//
 
-private:
+public:
 	int _hp = 100;
 };
 
-class Functor {
-public:
-	void operator() () {
-		cout << "Functor Test" << endl;
-		cout << _value << endl;
-	}
+template<typename T>
+void Print(T a) {
+	cout << a << endl;
+}
 
-	bool operator() (int num) {
-		cout << "Functor Test" << endl;
-		_value += num;
-		cout << _value << endl;
+// 템플릿 특수화
+template<>
+void Print(Knight a) {
+	cout << " 나이트!!!!!!!!!!!!!!!!" << endl;
+	cout << a._hp << endl;
+}
 
-		return true;
-	}
+template<typename T1, typename T2>
+void Print(T1 a, T2 b) {
+	cout << a << " " << b << endl;
+}
 
-private:
-	int _value = 0;
-};
+template<typename T>
+T Add(T a, T b) {
 
-class MoveTask {
-public:
-	void operator()() {
-		cout << "해당 좌표로 플레이어 이동" << endl;
-	}
+	return a + b;
+}
 
-public:
-	int _playerId;
-	int _posX;
-	int _posY;
-};
+
+
+void Print(int a) {
+	cout << a << endl;
+}
+
+void Print(float a) {
+	cout << a << endl;
+}
+
+// 연산자 오버로딩 (전역함수 버전)
+ostream& operator<<(ostream& os, const Knight& k) {
+	os << k._hp;
+	return os;
+}
 
 int main()
 {
-	// 함수 객체 : 함수처럼 동작하는 객체
-	// 함수 포인터의 단점
-	// 함수 포인터의 선언
+	// 템플릿 : 함수나 클래스를 찍어내는 틀
+	// 1) 함수 템플릿
+	// 2) 클래스 템플릿
 
+	Print<int>(50);
+	Print(50.0f);
+	Print(50.0);
+	Print("Hello World");
 
-	void (*pfunc)();
+	int result1 = Add(1, 2);
+	float result2 = Add<float>(1.1f, 1.2f);
 
-	pfunc = &HelloWorld;
-	(*pfunc)();
-
-	// 함수 포인터 단점
-	// 1) 시그니처가 안맞으면 사용 불가!
-	// 2) 상태를 가질 수 없다.
-
-	//pfunc = &HelloNumber;
-
-	// [함수처럼 동작]하는 객체
-	// ()연산자 오버로딩
-
-	HelloWorld();
-
-	Functor functor;
-	functor();
-	bool ret = functor(3);
-
-	// MMO에서 함수 객체를 사용하는 예시
-	// 클라 <-> 서버
-	// 서버 : 클라가 보내준 네트워크 패킷을 받아서 처리
-	// ex) 클라 : 나 (5,0) 좌표로 보내줘.
-	MoveTask task;
-	task._playerId = 100;
-	task._posX = 5;
-	task._posY = 0;
-
-	// 나중에 여유 될 때 일감 실행
-	task();
+	Knight k1;
+	Print(k1);
 
 	return 0;
 }
