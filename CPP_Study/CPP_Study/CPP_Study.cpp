e738196e039a7ae08ced57a913e538fff660d2c0
Yong Ju Kwak <amese1225@gmail.com>
2021-03-11 18:40:28 +0900

    템플릿 기초 #2 (클래스 템플릿, 템플릿 특수화)

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -3,69 +3,58 @@ using namespace std;
 
 // 오늘의 주제 : 템플릿 기초
 
-class Knight {
+// typename T를 붙이면 '조커카드' (어떤 타입도 다 넣을 수 있음)
+// 근데 무조건 TYPENAME을 붙여야 하는 것은 아니다.
+// template<> 안에 들어가는건 [골라줘야 하는 목록]
+
+template<typename T, int SIZE = 10>
+class RandomBox {
 public:
-	//
+	T GetRandomData() {
+		int idx = rand() % SIZE;
+		return _data[idx];
+	}
 
 public:
-	int _hp = 100;
+	T _data[10];
 };
 
-template<typename T>
-void Print(T a) {
-	cout << a << endl;
-}
-
 // 템플릿 특수화
-template<>
-void Print(Knight a) {
-	cout << " 나이트!!!!!!!!!!!!!!!!" << endl;
-	cout << a._hp << endl;
-}
+template<int SIZE>
+class RandomBox<double, SIZE> 
+{
+public:
+	double GetRandomData() {
+		cout << "RandomBox Double" << endl;
+		int idx = rand() % SIZE;
+		return _data[idx];
+	}
 
-template<typename T1, typename T2>
-void Print(T1 a, T2 b) {
-	cout << a << " " << b << endl;
-}
-
-template<typename T>
-T Add(T a, T b) {
-
-	return a + b;
-}
-
-
-
-void Print(int a) {
-	cout << a << endl;
-}
-
-void Print(float a) {
-	cout << a << endl;
-}
-
-// 연산자 오버로딩 (전역함수 버전)
-ostream& operator<<(ostream& os, const Knight& k) {
-	os << k._hp;
-	return os;
-}
+public:
+	double _data[SIZE];
+};
 
 int main()
 {
 	// 템플릿 : 함수나 클래스를 찍어내는 틀
 	// 1) 함수 템플릿
 	// 2) 클래스 템플릿
+	srand(static_cast<unsigned int>(time(nullptr)));
+	RandomBox<int, 10> rb1;
+	for (int i = 0; i < 10; i++) {
+		rb1._data[i] = i;
+	}
 
-	Print<int>(50);
-	Print(50.0f);
-	Print(50.0);
-	Print("Hello World");
+	int value1 = rb1.GetRandomData();
+	cout << value1 << endl;
 
-	int result1 = Add(1, 2);
-	float result2 = Add<float>(1.1f, 1.2f);
-
-	Knight k1;
-	Print(k1);
+	RandomBox<double, 20> rb2;
+	for (int i = 0; i < 20; i++) {
+		rb2._data[i] = i + 0.5;
+	}
+	double value2 = rb2.GetRandomData();
+	cout << value2 << endl;
+	
 
 	return 0;
 }
