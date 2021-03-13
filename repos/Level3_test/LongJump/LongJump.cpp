7a0c8f8a96da84d144959f737c16f17d6519594f
Yong Ju Kwak <amese1225@gmail.com>
2021-03-13 21:47:05 +0900

    멀리 뛰기

---
+++ repos/Level3_test/LongJump/LongJump.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int n) {
+    vector<long long> memo(n + 1, 0);
+    memo[1] = 1;
+    memo[2] = 2;
+
+    for (int i = 3; i <= n; i++) {
+        memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+    }
+
+    return memo[n];
+}
+
+int main()
+{
+    __int64 ans = solution(4);
+    cout << ans << endl;
+
+    std::cout << "Hello World!\n";
+}
