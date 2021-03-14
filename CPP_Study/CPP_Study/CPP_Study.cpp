ee16bc1d87ea231c6356efa1aebe6a992a63b524
Yong Ju Kwak <amese1225@gmail.com>
2021-03-14 15:36:53 +0900

    list #3 (list 구현)

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -5,71 +5,175 @@ using namespace std;
 
 // 오늘의 주제 : list
 
+template<typename T>
 class Node {
 public:
-	Node* _next;
-	Node* _prev;
-	int _data;
+	Node() : _next(nullptr), _prev(nullptr), _data(T()) {
+
+	}
+
+	Node(const T& value) : _next(nullptr), _prev(nullptr), _data(value) {
+
+	}
+
+public:
+	Node*	_next;
+	Node*	_prev;
+	T		_data;
+};
+
+template<typename T>
+class Iterator {
+public:
+	Iterator() : _node(nullptr) {
+
+	}
+
+	Iterator(Node<T>* node) : _node(node) {
+
+	}
+
+	Iterator<T>& operator++() {
+		_node = _node->_next;
+		return *this;
+	}
+
+	Iterator<T> operator++(int) {
+		Iterator<T> temp = *this;
+		_node = _node->next;
+		return temp;
+	}
+
+	Iterator<T>& operator--() {
+		_node = _node->_prev;
+		return *this;
+	}
+
+	Iterator<T> operator--(int) {
+		Iterator<T> temp = *this;
+		_node = _node->_prev;
+		return temp;
+	}
+
+	T& operator*() {
+		return _node->_data;
+	}
+
+	bool operator==(const Iterator& right) {
+		return _node == right._node;
+	}
+
+	bool operator!=(const Iterator& right) {
+		return _node != right._node;
+	}
+
+public:
+	// Containner
+	Node<T>* _node;
+};
+
+// [ header ]
+template<typename T>
+class List {
+public:
+	List() : _size(0) {
+		_header = new Node<T>();
+		_header->_next = _header;
+		_header->_prev = _header;
+	}
+
+	~List() {
+		// TODO
+		while (_size > 0)
+			pop_back();
+
+		delete _header;
+	}
+
+	void push_back(const T& value) {
+		AddNode(_header, value);
+	}
+
+	void pop_back() {
+		RemoveNode(_header->_prev);
+	}
+
+	// <-> [ header ] <->
+
+	// [1] <-> [2] <-> [before] <-> [4] <-> header <->
+	// [1] <-> [2] <-> [value] <-> [before] <-> [4] <-> header <->
+
+	Node<T>* AddNode(Node<T>* before, const int& value) {
+		Node<T>* node = new Node<T>(value);
+
+		Node<T>* prevNode = before->_prev;
+		prevNode->_next = node;
+		node->_prev = prevNode;
+
+		node->_next = before;
+		before->_prev = node;
+
+		_size++;
+
+		return node;
+	}
+
+	Node<T>* RemoveNode(Node<T>* node) {
+		Node<T>* prevNode = node->_prev;
+		Node<T>* nextNode = node->_next;
+
+		prevNode->_next = nextNode;
+		nextNode->_prev = prevNode;
+
+		delete node;
+		_size--;
+
+		return nextNode;
+	}
+
+	int Size() { return _size; }
+
+public:
+	typedef Iterator<T> iterator;
+	iterator begin() { return iterator(_header->_next); }
+	iterator end() { return iterator(_header); }
+
+	iterator insert(iterator it, const T& value) {
+		Node<T>* node = AddNode(it._node, value);
+		return iterator(node);
+	}
+
+	iterator erase(iterator it) {
+		Node<T>* node = RemoveNode(it._node);
+		return iterator(node);
+	}
+
+private:
+	Node<T>* _header;
+	int _size;
 };
 
 int main()
 {
-	// list (연결 리스트)
-	// - list의 동작 원리
-	// - 중간 삽입/삭제
-	// - 처음/끝 삽입/삭제
-	// - 임의 접근
+	List<int> li;
+	List<int>::iterator eraseIt;
 
-	list<int> li;
+	for (int i = 0; i < 10; i++) {
 
-	list<int>::iterator itRemember;
-	for (int i = 0; i < 100; i++) {
-
-		if (i == 50) {
-			itRemember = li.insert(li.end(), i);
+		if (i == 5) {
+			eraseIt = li.insert(li.end(), i);
 		}
 		else {
 			li.push_back(i);
 		}
-
-
 	}
 
+	li.pop_back();
+	li.erase(eraseIt);
 
-	for (int i = 0; i < 100; i++)
-		li.push_back(0);
-
-	int size = li.size();
-	
-	int first = li.front();
-	int last = li.back();
-
-	// 임의 접근 비허용
-	//li[3] = 10; // 없음
-
-	list<int>::iterator itBegin = li.begin();
-	list<int>::iterator itEnd = li.end();
-
-
-	//list<int>::iterator itTest1 = --itBegin;
-	//list<int>::iterator itTest2 = --itEnd;
-	//list<int>::iterator itTest3 = ++itEnd;
-
-	int* ptrBegin = &(li.front());
-	int* ptrEnd = &(li.back());
-
-
-	for (list<int>::iterator it = li.begin(); it != li.end(); it++) {
-		cout << *it << endl;
+	for (List<int>::iterator it = li.begin(); it != li.end(); ++it) {
+		cout << (*it) << endl;
 	}
-
-	li.insert(itBegin, 100);
-
-	li.erase(li.begin());
-
-	li.pop_front();
-
-	li.remove(10);
 
 
 	return 0;
