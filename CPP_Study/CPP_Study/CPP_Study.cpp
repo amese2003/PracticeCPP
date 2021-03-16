aa305d982852ca3cf13b0fd88cbbce88b358df7f
Yong Ju Kwak <amese1225@gmail.com>
2021-03-16 15:31:06 +0900

    AUTO

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -9,171 +9,53 @@
 using namespace std;
 
 // 오늘의 주제 : 연습문제
+
+class Knight {
+
+};
+
 int main()
 {
-	srand(static_cast<unsigned int>(time(nullptr)));
+
+	auto a = 3;
+	auto b = 3.14f;
+	auto c = 1.23;
+	auto d = Knight();
+	auto e = "Nero";
+
+	// auto는 일종의 조커카드
+	// 형식 연역 (type deduction)
+	// -> 말이 되게 잘 맞춰봐라. (추론)
+	// 추론 규칙은 생각보다 복잡해질 수 있다.
+
+	auto f = &d;
+	const auto test1 = b;
+	auto* test2 = e;
+
+	// 주의!
+	// 기본 auto는 const, & 무시!!!!!!!!!!!
+
+	int& reference = a;
+	const int cst = a;
+	
+	auto test1 = reference;
+	auto test2 = cst;
 
 	vector<int> v;
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
 
-	for (int i = 0; i < 100; i++) {
-		int num = rand() % 100;
-		v.push_back(num);
+	for (vector<int>::size_type i = 0; i < v.size(); i++) {
+		int& data = v[i];
+
+		data = 100;
 	}
 
-	// Q1) number라는 숫자가 벡터에 체크하는 기능 (bool? 첫등장 iterator?)
-	{
-		int number = 50;
-
-		bool found = false;
-		vector<int>::iterator itFind;
-
-		for (int i = 0; i < v.size(); i++) {
-			int data = v[i];
-
-			if (data == number) {
-				found = true;
-				itFind = v.begin() + i;
-				break;
-			}
-		}
-		
-		//it = v.begin();
-		for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-			if ((*it) == number) {
-				found = true;
-				itFind = it;
-				break;
-			}
-		}
-
-		itFind = find(v.begin(), v.end(), number);
-
-		if (itFind == v.end()) {
-			cout << "못찾음" << endl;
-		}
-		else {
-			cout << "찾음" << endl;
-		}
-
-
-		int b = 3;
-	}
-
-	// Q2) 11로 나뉘는 숫자가 벡터에 있는지?
-	{
-		bool found = false;
-		vector<int>::iterator itFind;
-
-		for (int i = 0; i < v.size(); i++) {
-			int data = v[i];
-
-			if (data % 11 == 0) {
-				found = true;
-				itFind = v.begin() + i;
-				break;
-			}
-		}
-
-		for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-			if ((*it) % 11 == 0) {
-				found = true;
-				itFind = it;
-				break;
-			}
-		}
-
-		struct CanDivideBy11 {
-			bool operator() (int n) {
-				return (n % 11 == 0);
-			}
-		};		
-
-		itFind = find_if(v.begin(), v.end(), CanDivideBy11());
-		//itFind = find_if(v.begin(), v.end(), [](int n) {return n % 11 == 0; });
-
-		if (itFind == v.end()) {
-			cout << "못찾음" << endl;
-		}
-		else {
-			cout << "찾음" << endl;
-		}
-	}
-
-	// Q3) 홀수인 숫자의 개수는? (count)
-	{
-		vector<int>::iterator itFind;
-		int count = 0;
-		for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-			if ((*it) % 2 == 1) {
-				count++;
-				break;
-			}
-		}
-
-		struct IsOdd {
-			bool operator()(int n) {
-				return (n % 2) != 0;
-			}
-		};
-
-		int n = std::count_if(v.begin(), v.end(), IsOdd());
-
-		// 모든 데이터가 홀수?
-		bool b1 = std::all_of(v.begin(), v.end(), IsOdd());
-		// 홀수인 데이터가 적어도 하나라도 있나?
-		bool b2 = std::any_of(v.begin(), v.end(), IsOdd());
-		// 모든 데이터가 홀수가 아닌가?
-		bool b3 = std::none_of(v.begin(), v.end(), IsOdd());
-		
-	}
-
-	// Q4) 벡터에 들어가 있는 모든 숫자에 3을 곱해주세요!
-	{
-		/*for (vector<int>::iterator it = v.begin(); it != v.end(); ++it) {
-			*it = (*it) * 3;
-		}*/
-
-		for (unsigned int i = 0; i < v.size(); i++) {
-			v[i] *= 3;
-		}
-
-		struct MultiplyBy3 {
-			void operator()(int& n) {
-				n = n * 3;
-			}
-		};
-
-
-
-		std::for_each(v.begin(), v.end(), MultiplyBy3());
-	}
-
-	// 홀수인 데이터를 일괄 삭제
-	{
-		vector<int>::iterator itFind;
-
-		v.clear();
-
-		v.push_back(1);
-		v.push_back(4);
-		v.push_back(3);
-		v.push_back(5);
-		v.push_back(8);
-		v.push_back(2);
-
-		struct IsOdd {
-			bool operator()(int n) {
-				return (n % 2) != 0;
-			}
-		};
-
-		//std::remove(v.begin(), v.end(), 4);
-
-		itFind = std::remove_if(v.begin(), v.end(), IsOdd());
-		v.erase(itFind, v.end());
-
-		int a = 3;
-	}	
+	// 아무튼 이제 기존의 타입은 잊어버리고 auto만 쓴다?
+	// NO!
+	// -> 타이핑이 길어지는 경우 OK
+	// -> 가독성을 위해 일반적으로는 놔두는게 좋음
 	
 	return 0;
 }
