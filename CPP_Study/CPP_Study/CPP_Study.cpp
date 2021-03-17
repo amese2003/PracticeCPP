1a753a9a7f594f00449a413d9a1b1039aac7766c
Yong Ju Kwak <amese1225@gmail.com>
2021-03-17 11:41:13 +0900

    오른값 참조

--- CPP_Study/CPP_Study/CPP_Study.cpp
+++ CPP_Study/CPP_Study/CPP_Study.cpp
@@ -8,37 +8,98 @@
 
 using namespace std;
 
-// 오늘의 주제 : override, final
+// 오늘의 주제 : 오른값(rvalue) 참조와 std::move
 
-class Creature {
-public:
-	virtual void Attack() {
-		cout << "Player!" << endl;
-	}
-};
-class Player : public Creature {
-public:
-	virtual void Attack() override {
-		cout << "Player!" << endl;
-	}
+class Pet {
+
 };
 
-class Knight : public Player {
+
+class Knight {
 public:
-	virtual void Attack() override {
-		cout << "Knight!" << endl;
+
+	Knight() {
+		cout << "Knight()" << endl;
 	}
 
-private:
-	int _stamina = 100;
+	Knight(const Knight& knight) {
+		cout << "const Knight&" << endl;
+	}
+
+	// 이동 생성자
+	Knight(Knight&& knight) {
+
+	}
+
+	~Knight() {
+		if (_pet)
+			delete _pet;
+	}
+
+	// 복사 대입 연산자
+	void operator=(const Knight& knight) {
+		cout << "operator=(const Knight& knight)" << endl;
+
+		_hp = knight._hp;
+
+		if (knight._pet)
+			_pet = new Pet(*knight._pet);		
+	}
+
+	// 이동 대입 연산자
+	void operator=(Knight&& knight) noexcept {
+		cout << "operator=(Knight&&)" << endl;
+
+		// 얕은 복사
+		_hp = knight._hp;
+		_pet = knight._pet;
+
+		knight._pet = nullptr;
+	}
+
+
+public:
+	int _hp = 100;
+	Pet* _pet = nullptr;
 };
 
+void TestKnight_Copy(Knight knight) {}
+void TestKnight_LValueRef(Knight& knight) {}
+void TestKnight_ConstValueRef(const Knight& knight) {  }
+void TestKnight_RValueRef(Knight&& knight) {} // 이동 대상!
 
 int main()
 {
-	Player* p = new Knight();
+	// 왼값(lvalue) vs 오른값(rvalue)
+	// - lvalue : 단일식을 넘어서 계속 지속되는 개체
+	// - rvalue = lvalue가 아닌 나머지 (임시 값, 열거형, 람다, i++ 등...)
 
-	p->Attack();
+	int a = 3;
+	
+	Knight k1;
+	TestKnight_Copy(k1);
+	TestKnight_LValueRef(k1);
+	//TestKnight_LValueRef(Knight());
+
+
+	TestKnight_ConstValueRef(Knight());
+
+	TestKnight_RValueRef(Knight());
+	TestKnight_RValueRef(static_cast<Knight&&>(k1));
+
+	Knight k2;
+	k2._pet = new Pet();
+	k2._hp = 1000;
+
+	// 원본은 날려도 된다. << hint를 주는 쪽에 가깝다!
+	Knight k3;
+	k3 = static_cast<Knight&&>(k2);
+
+	k3 = std::move(k2);
+	// std::move의 본래 이름 후보 중 하나가 rvalue_cast
+
+	std::unique_ptr<Knight> uptr = std::make_unique<Knight>();
+	std::unique_ptr<Knight> uptr2 = std::move(uptr);
 
 	return 0;
 }
