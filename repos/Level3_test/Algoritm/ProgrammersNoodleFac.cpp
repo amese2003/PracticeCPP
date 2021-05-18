471bcf73f8aae4da3ff0fef41b620f28d9a06243
용주 곽 <amese1225@gmail.com>
2021-05-18 22:17:47 +0900

    프로그래머스 - 리먄공장

---
+++ repos/Level3_test/Algoritm/ProgrammersNoodleFac.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int Noodle(int stock, vector<int> dates, vector<int> supplies, int k) {
+	int receive_count = 0;
+	int day_num = 0;
+	priority_queue<int> supply_receive;
+
+	for (int i = 0; i < k; i++) {
+		if (dates[day_num] == i) {
+			supply_receive.push(supplies[day_num]);
+			day_num++;
+		}
+
+		if (stock == 0) {
+			stock = supply_receive.top();
+			supply_receive.pop();
+			receive_count++;
+		}
+
+		stock--;
+	}
+
+	return receive_count;
+}
