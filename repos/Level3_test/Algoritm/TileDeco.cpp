a45949cafe179cd152a8b9e7e17e6a9f3c831e10
용주 곽 <amese1225@gmail.com>
2021-05-29 21:54:27 +0900

    타일링

---
+++ repos/Level3_test/Algoritm/TileDeco.cpp
@@ -0,0 +1,16 @@
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution(int N) {
+	vector<long long> side_length(2, 1);
+
+	for (int i = 2; i < N; i++) {
+		long long new_length = side_length[i - 1] + side_length[i - 2];
+		side_length.push_back(new_length);
+	}
+
+	return (side_length[N - 1] + (side_length[N - 1] + side_length[N - 2])) * 2;
+}
