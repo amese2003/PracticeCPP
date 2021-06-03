00e96a4ab6977c3527966212b57456e79540c1de
Yong Ju Kwak <amese1225@gmail.com>
2021-06-03 23:56:01 +0900

    합승택시요금

---
+++ repos/Level3_test/Algoritm/TotalTexi.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+const int NodeCount = 201;
+int Node[NodeCount][NodeCount];
+
+int TotalTexi(int n, int s, int a, int b, vector<vector<int>> fares) {
+    int answer = 1e9;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (j == i)
+                Node[i][j] = 0;
+            else
+                Node[i][j] = 10000000;
+        }
+    }
+
+    for (auto fare : fares) {
+        Node[fare[0]][fare[1]] = fare[2];
+        Node[fare[1]][fare[0]] = fare[2];
+    }
+
+
+    for (int pivot = 1; pivot <= n; pivot++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                Node[i][j] = min(Node[i][j], Node[i][pivot] + Node[pivot][j]);
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++)
+        answer = min(answer, Node[s][i] + Node[i][a] + Node[i][b]);
+
+    return answer;
+}
