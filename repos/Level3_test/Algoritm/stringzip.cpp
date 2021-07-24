c0b8acc415d18a61f9f2cb1cf6076dc01e481355
용주 곽 <amese1225@gmail.com>
2021-07-24 23:52:05 +0900

    문자열 압축

---
+++ repos/Level3_test/Algoritm/stringzip.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int stringZip(string s) {
+	int answer = s.length();
+
+	string slice = "";
+	string next_slice = "";
+	int zipCount = 1;
+
+	vector<int> zipSize;
+
+	for (int i = 1; i <= s.length() / 2; i++) {
+		string makeString = "";
+
+		for (int j = 0; j < s.length(); j += i) {
+
+			slice = s.substr(j, i);
+
+			if (j + i > s.length() || slice != s.substr(j + i, i)) {
+				if (zipCount > 1) {
+					makeString += to_string(zipCount);
+
+				}
+				makeString += slice;
+				zipCount = 1;
+			}
+			else
+				zipCount++;
+		}
+
+		zipSize.push_back(makeString.length());
+	}
+
+
+	if (s.length() == 1)
+		return 1;
+	else
+		return *min_element(zipSize.begin(), zipSize.end());
+}
