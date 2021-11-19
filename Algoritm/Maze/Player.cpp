08148ad2afb6fc9be023ccfa26d307e112573b1e
용주 곽 <amese1225@gmail.com>
2021-11-19 19:53:22 +0900

    오른손 법칙 개선

--- Algoritm/Maze/Player.cpp
+++ Algoritm/Maze/Player.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Player.h"
 #include "Board.h"
+#include <stack>
 
 void Player::Init(Board* board)
 {
@@ -49,6 +50,32 @@ void Player::Init(Board* board)
 			
 		}
 	}
+
+	stack<Pos> s;
+	for (int i = 0; i < _path.size() - 1; i++) 
+	{
+
+		if (s.empty() == false && s.top() == _path[i + 1])
+			s.pop();
+		else
+			s.push(_path[i]);
+	}
+
+	if (_path.empty() == false)
+		s.push(_path.back());
+
+	vector<Pos> path;
+
+	while (s.empty() == false)
+	{
+		path.push_back(s.top());
+		s.pop();
+	}
+
+	std::reverse(path.begin(), path.end());
+
+
+	_path = path;
 }
 
 void Player::Update(uint64 deltaTick)
