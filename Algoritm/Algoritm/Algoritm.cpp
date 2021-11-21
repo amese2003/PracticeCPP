52356bb0695804fb7566b978aba77e0b61eb2582
용주 곽 <amese1225@gmail.com>
2021-11-21 22:38:50 +0900

    DFS

--- Algoritm/Algoritm/Algoritm.cpp
+++ Algoritm/Algoritm/Algoritm.cpp
@@ -3,129 +3,82 @@
 
 using namespace std;
 
-
-void CreateGraph_1()
+struct Vertex
 {
-	struct Node
+
+};
+
+vector<Vertex> vertices;
+vector<vector<int>> adjacent;
+vector<bool> visited;
+
+void CreateGraph()
+{
+	vertices.resize(6);
+	adjacent = vector<vector<int>>(6);
+
+	/*adjacent[0].push_back(1);
+	adjacent[0].push_back(3);
+	adjacent[1].push_back(0);
+	adjacent[1].push_back(2);
+	adjacent[1].push_back(3);
+	adjacent[3].push_back(4);
+	adjacent[5].push_back(4);*/
+
+
+	adjacent = vector<vector<int>>
 	{
-		Node* prev;
-		Node* next;
-		int data;
+		{0,1,0,1,0,0},
+		{1,0,1,1,0,0},
+		{0,1,0,1,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,0,0},
+		{0,0,0,0,1,0}
 	};
 
-	struct Vertex
+}
+
+void Dfs(int here)
+{
+	visited[here] = true;
+
+	cout << "visited : " << here << endl;
+
+	/*for (int i = 0; i < adjacent[here].size(); i++)
 	{
-		vector<Vertex*> edges;
-	};
+		int there = adjacent[here][i];
 
-	vector<Vertex> v;
-	v.resize(0);
+		if (visited[there] == false)
+			Dfs(there);
+	}*/
 
-	v[0].edges.push_back(&v[1]);
-	v[0].edges.push_back(&v[3]);
-	v[1].edges.push_back(&v[0]);
-	v[1].edges.push_back(&v[2]);
-	v[1].edges.push_back(&v[3]);
-	v[3].edges.push_back(&v[4]);
-	v[5].edges.push_back(&v[4]);
 
-	bool check = false;
-	for (Vertex* edge : v[0].edges)
+	for (int there = 0; there < 6; there++)
 	{
-		if (edge == &v[3])
-		{
-			check = true;
-			break;
-		}
+		if (adjacent[here][there] == 0)
+			continue;
+
+		if (visited[there] == false)
+			Dfs(there);
 	}
 
 }
 
-
-
-void CreateGraph_2()
+void DfsAll()
 {
-	struct Node
-	{
-		Node* prev;
-		Node* next;
-		int data;
-	};
+	visited = vector<bool>(6, false);
 
-	struct Vertex
-	{
-		vector<Vertex*> edges;
-	};
-
-	vector<Vertex> v;
-	v.resize(6);
-	
-	// 연결 목록을 따로 관리
-	vector<vector<int>> adjacent(6);
-
-
-	adjacent[0] = { 1,3 };
-	adjacent[1] = { 0,2,3 };
-	adjacent[3] = { 4 };
-	adjacent[5] = { 4 };
-
-	bool check = false;
-	for (int vertex : adjacent[0])
-	{
-		if (vertex == 3)
-		{
-			check = true;
-			break;
-		}
-	}
-
-	vector<int>& adj = adjacent[0];
-	bool find2 = (std::find(adj.begin(), adj.end(), 3) != adj.end());
+	for (int i = 0; i < 6; i++)
+		if (visited[i] == false)
+			Dfs(i);
 }
 
-
-
-
-void CreateGraph_3()
-{
-	struct Node
-	{
-		Node* prev;
-		Node* next;
-		int data;
-	};
-
-	struct Vertex
-	{
-		vector<Vertex*> edges;
-	};
-
-	vector<Vertex> v;
-	v.resize(6);
-
-	// 연결 목록을 따로 관리
-	vector<vector<bool>> adjacent(6, vector<bool>(6, false));
-	adjacent[0][1] = true;
-	adjacent[0][3] = true;
-	adjacent[1][0] = true;
-	adjacent[1][2] = true;
-	adjacent[1][3] = true;
-	adjacent[3][4] = true;
-	adjacent[5][4] = true;
-
-	vector<vector<int>> adjacent2 = 
-	{
-		vector<int> {-1, 15, -1, 35, -1, -1},
-		vector<int> {15, -1, 5, 10, -1, -1},
-		vector<int> {-1 ,-1 ,-1 ,-1 ,-1, -1},
-		vector<int> {-1 ,-1 ,-1 ,-1 ,5, -1},
-		vector<int> {-1 ,-1 ,-1 ,-1 ,-1, -1},
-		vector<int> {-1 ,-1 ,-1 ,-1 ,5, -1},
-	}
-
-}
 int main()
 {
+	CreateGraph();
 
+	
+
+	DfsAll();
 
 }
