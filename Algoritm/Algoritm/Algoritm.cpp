94cc00626c46e76b185b451a9bba17b8461ffad0
용주 곽 <amese1225@gmail.com>
2021-11-26 20:11:12 +0900

    c++ 우선순위 큐 구현

--- Algoritm/Algoritm/Algoritm.cpp
+++ Algoritm/Algoritm/Algoritm.cpp
@@ -5,107 +5,95 @@
 
 using namespace std;
 
-using NodeRef = shared_ptr<struct Node>;
-
-struct Node
+template<typename T, typename Container = vector<T>, typename Predicate = less<T>>
+class Priority_Queue
 {
-	Node() {}
-	Node(const string& data) : data(data) {}
+public:
+	void push(const T& data)
+	{
+		_heap.push_back(data);
 
-	string data;
-	vector<NodeRef> childern;
+		int now = static_cast<int>(_heap.size()) - 1;
+
+		while (now > 0)
+		{
+			int next = (now - 1) / 2;
+			/*if (_heap[now] < _heap[next])
+				break;*/
+
+			if (_predicate(_heap[now], _heap[next]))
+				break;
+
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	void Pop()
+	{
+		cout << _heap[0] << endl;
+		_heap[0] = _heap.back();
+		_heap.pop_back();
+
+		int now = 0;
+
+		while (true)
+		{
+			int left = now * 2 + 1;
+			int right = now * 2 + 2;
+
+			if (left >= _heap.size())
+				break;
+
+			int next = now;
+
+			if (_predicate(_heap[next], _heap[left]))
+				next = left;
+			
+			if (right < static_cast<int>(_heap.size()) && _predicate(_heap[next], _heap[right]))
+				next = right;
+
+			if (next == now)
+				break;
+
+			::swap(_heap[now], _heap[next]);
+			now = next;
+		}
+	}
+
+	T& Top()
+	{
+		return _heap[0];
+	}
+
+	bool empty()
+	{
+		return _heap.empty();
+	}
+
+private:
+	Container _heap = {};
+	Predicate _predicate = {};
 };
 
-NodeRef CreateTree()
-{
-	NodeRef root = make_shared<Node>("R1 개발부");
-	{
-		NodeRef node = make_shared<Node>("디자인팀");
-		root->childern.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("전투");
-			node->childern.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("경제");
-			node->childern.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("스토리");
-			node->childern.push_back(leaf);
-		}
-	}
-
-	{
-		NodeRef node = make_shared<Node>("프로그래밍팀");
-		root->childern.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("서버");
-			node->childern.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("클라");
-			node->childern.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("엔진");
-			node->childern.push_back(leaf);
-			{
-				NodeRef dummy = make_shared<Node>("더미");
-				leaf->childern.push_back(dummy);
-			}
-		}
-	}
-
-	{
-		NodeRef node = make_shared<Node>("아트팀");
-		root->childern.push_back(node);
-		{
-			NodeRef leaf = make_shared<Node>("배경");
-			node->childern.push_back(leaf);
-		}
-		{
-			NodeRef leaf = make_shared<Node>("캐릭터");
-			node->childern.push_back(leaf);
-		}
-	}
-
-
-	return root;
-}
-
-void PrintTree(NodeRef root, int depth)
-{
-	
-
-	for (int i = 0; i < depth; i++)
-		cout << "-";
-	cout << root->data << endl;
-
-	for (NodeRef& child : root->childern)
-		PrintTree(child, depth + 1);
-
-}
-
-int GetHeight(NodeRef root)
-{
-	int height = 1;
-
-	for (NodeRef& child : root->childern)
-	{
-		height = max(height, GetHeight(child) + 1);
-	}
-
-	return height;
-}
 
 int main()
 {
-	NodeRef root = CreateTree();
-	PrintTree(root, 0);
+	Priority_Queue<int, vector<int>, greater<int>> test;
 
-	int height = GetHeight(root);
-	cout << "Height : " << height << endl;
+	test.push(100);
+	test.push(101);
+	test.push(102);
+	test.push(103);
+	test.push(104);
+
+	test.Pop();
+	test.Pop();
+	test.Pop();
+	test.Pop();
+	test.Pop();
+
+
 
 	return 0;
 }
