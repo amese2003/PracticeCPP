865627de9b15ee2e05facffc3a1af5eaa299f799
용주 곽 <amese1225@gmail.com>
2021-12-08 22:59:30 +0900

    Prim

--- Algoritm/Maze/Board.cpp
+++ Algoritm/Maze/Board.cpp
@@ -21,7 +21,7 @@ void Board::Init(int32 size, Player* player)
 	_size = size;
 	_player = player;
 
-	GenerateMap();
+	GenerateMap_Prim();
 }
 
 void Board::Render()
@@ -44,8 +44,22 @@ void Board::Render()
 
 // Binary Tree 미로 생성 알고리즘
 // - Mazes For Programmers
-void Board::GenerateMap()
+void Board::GenerateMap_Kruskal()
 {
+
+	struct CostEdge
+	{
+		int cost;
+		Pos u;
+		Pos v;
+
+		bool operator<(CostEdge& other)
+		{
+			return cost < other.cost;
+		}
+	};
+
+
 	for (int32 y = 0; y < _size; y++)
 	{
 		for (int32 x = 0; x < _size; x++)
@@ -69,12 +83,16 @@ void Board::GenerateMap()
 
 			if (x < _size - 2)
 			{
+				// [][벽][]
 				const int32 randvalue = ::rand() % 100;
 				edges.push_back(CostEdge{ randvalue, Pos{y,x}, Pos{y, x + 2} });
 			}
 
 			if (y < _size - 2)
 			{
+				// [  ]
+				// [벽]
+				// [  ]
 				const int32 randValue = ::rand() % 100;
 				edges.push_back(CostEdge{ randValue, Pos{y, x}, Pos{y + 2,x} });
 			}
@@ -100,6 +118,131 @@ void Board::GenerateMap()
 		int x = (edge.u.x + edge.v.x) / 2;
 		_tile[y][x] = TileType::EMPTY;
 	}
+}
+
+void Board::GenerateMap_Prim()
+{
+	struct CostEdge
+	{
+		int cost;
+		Pos vtx;
+
+		bool operator<(const CostEdge& other) const
+		{
+			return cost < other.cost;
+		}
+	};
+
+
+	for (int32 y = 0; y < _size; y++)
+	{
+		for (int32 x = 0; x < _size; x++)
+		{
+			if (x % 2 == 0 || y % 2 == 0)
+				_tile[y][x] = TileType::WALL;
+			else
+				_tile[y][x] = TileType::EMPTY;
+		}
+	}
+
+	// edges[u] : u 정점과 연결된 간선
+	map<Pos, vector<CostEdge>> edges;
+
+	// edges 후보를 랜덤 cost로 등록
+	for (int32 y = 0; y < _size; y++)
+	{
+		for (int32 x = 0; x < _size; x++)
+		{
+			if (x % 2 == 0 || y % 2 == 0)
+				continue;
+
+			if (x < _size - 2)
+			{
+				// [][벽][]
+				const int32 randValue = ::rand() % 100;
+				Pos u = Pos{ y,x };
+				Pos v = Pos{ y, x + 2 };
+				edges[u].push_back(CostEdge{ randValue, v });
+				edges[v].push_back(CostEdge{ randValue, u });
+			}
+
+			if (y < _size - 2)
+			{
+				// [  ]
+				// [벽]
+				// [  ]
+				const int32 randValue = ::rand() % 100;
+				Pos u = Pos{ y,x };
+				Pos v = Pos{ y + 2, x };
+				edges[u].push_back(CostEdge{ randValue, v });
+				edges[v].push_back(CostEdge{ randValue, u });
+			}
+		}
+	}
+
+	// 해당 정점이 트리에 포함되어 있을까?
+	map<Pos, bool> added;
+
+	// 어떤 정점이 누구에 의해 연결 되어있는지
+	map<Pos, Pos> parent;
+	// 만들고 있는 트리에 인접한 간선 중, 해당 정점에 닿는 최소 간선의 정보
+	map<Pos, int32> best;
+
+	// 다익스트라랑 거~의 유사.
+	// - 다익스트라는 best가 [시작점]을 기준으로 한 cost
+	// - 프림에서는 best가 [현재 트리]를 기준으로 한 간선 cost
+
+	for (int32 y = 0; y < _size; y++)
+	{
+		for (int32 x = 0; x < _size; x++)
+		{
+			best[Pos{ y,x }] = INT32_MAX;
+			added[Pos{ y, x }] = false;
+		}
+	}
+
+	priority_queue<CostEdge> pq;
+	const Pos startPos = Pos{ 1,1 };
+	pq.push(CostEdge{ 0, startPos });
+	best[startPos] = 0;
+	parent[startPos] = startPos;
+
+
+	while (pq.empty() == false)
+	{
+		CostEdge bestEdge = pq.top();
+		pq.pop();
+
+		// 새로 연결된 정점
+		Pos v = bestEdge.vtx;
+		// 이미 추가 되었다면 스킵
+		if (added[v])
+			continue;
+
+		added[v] = true;
+
+		// 맵에 적용
+		{
+			int y = (parent[v].y + v.y) / 2;
+			int x = (parent[v].x + v.x) / 2;
+			_tile[y][x] = TileType::EMPTY;
+		}
+
+		for (CostEdge& edge : edges[v])
+		{
+			if (added[edge.vtx])
+				continue;
+
+			// 다른 경로로 인해서 더 좋은 경로가 발견되면 스킵
+			if (edge.cost > best[edge.vtx])
+				continue;
+
+			best[edge.vtx] = edge.cost;
+			parent[edge.vtx] = v;
+			pq.push(edge);
+		}
+	}
+
 }
 
 TileType Board::GetTileType(Pos pos)
